b7e012c4dc5db21d4e2e98afbd58123f7adb9cc8
ubernx <n203313@gmail.com>
2025-09-24 19:24:46 +0200

    Upload

--- src/Main.java
+++ src/Main.java
@@ -1,193 +1,206 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
 
-    public static final String CMD_EXIT = "/exit";
-    public static final String CMD_GENERATE = "/generate";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String INVALID_CMD = "Unbekannte Anweisung";
-    public static final String ERROR_POSITIVE_INTEGER = "Sie dürfen nur positive ganze Zahlen verwenden";
-    public static final String ERROR_LENGTH = "Sie haben den Rahmen von 4-16 Zeichen pro Passwort überschritten";
-    public static final String ERROR_MODE = "Ungültiger Modus ausgewählt.";
-    public static final String WELCOME = "\nWillkommen zum Passwort-Generator.";
-    public static final String ASK_LENGTH = "Bitte geben Sie die gewünschte Länge des Passworts an: ";
-    public static final String ASK_COUNT = "Bitte geben Sie die gewünschte Anzahl der Passwörter an: ";
-    public static final String ASK_MODE = "Modus (1 oder 2): ";
-    public static final String MENU_HELP = """
-            Folgende Commands sind verfügbar:
-            '/generate' (Generiert eine gewählte Anzahl von Passwörtern, mit der gewählten Länge und der gewählten Zeichenart)
-            '/exit'     (Beendet und schließt das Programm)
-            """;
-
-    public static final String MODE_SELECTION = """
+    private static final int SKIP_INTEGERS = 10;
+    private static final String RED = "\u001B[31m";
+    private static final String RESET = "\u001B[0m";
+    private static final String COMMAND_GENERATE = "/generate";
+    private static final String COMMAND_EXIT = "/exit";
+    private static final String COMMAND_ERROR = RED + " Befehl existiert nicht, geben Sie erneut den Befehl ein:" + RESET;
+    private static final String REQUEST_PASSWORD_COUNT = "Bitte geben Sie die gewünschte Anzahl der Passworter an: ";
+    private static final String REQUEST_PASSWORD_LENGTH = "Bitte geben Sie die gewünschte Passwort länge an: ";
+    private static final String ERROR_NOT_AN_POSITIVE_INTEGER = RED + "Sie dürfen nur positive ganze Zahlen verwenden" + RESET;
+    private static final String ERROR_OUT_OF_BOUND_PASSWORD_LENGTH = RED + "Sie haben den Zeichenlänge bereich <4-16> nicht eingehalten" + RESET;
+    private static final String ERROR_MODE_TYPE = RED + "1 und 2 sind die einzigen Gültigen Modi" + RESET;
+    private static final String PRINT_OR_SAVE_PASSWORDS = "Passwörter in Datei speichern? (J/N): ";
+    private static final String WRITE_SUCCEEDED = "Passwörter befinden sich in: ";
+    private static final String WRITE_FAILED = RED + "Failed to write config file:" + RESET;
+    private static final String PASSWORDS_FILENAME = "pw.txt";
+    private static final String LINEBREAK = "\n";
+    private static final String J = "J";
+    private static final String EMPTY_SPACEHOLDER = "";
+    private static final String QUOTES = "''";
+    private static final String NUMMERATOR = ". ";
+    private static final String ALPHANUMMERICAL = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String SPECIAL = "#$%&'()*+,-./";
+    private static final String REQUEST_PASSWORD_MODE = """
             Bitte wählen Sie den Modus:
             1: Groß- und Kleinbuchstaben + Zahlen
             2: Groß- und Kleinbuchstaben + Zahlen + Sonderzeichen
             """;
+    private static final String MAIN_MENU = """
+        Folgende Commands sind verfügbar:
+        '/generate' (Generiert eine gewählte Anzahl von Passwörtern, mit der gewählten Länge und der gewählten Zeichenart)
+        '/exit'     (Beendet und schließt das Programm)
+        """;
 
-    public static long PW_GENERATED_TIME = 0;
-    public static long PW_TOSTRING_TIME = 0;
+
+    static Scanner parameters = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        Scanner commandInput = new Scanner(System.in);
-        System.out.print(WELCOME);
-
-        int i = 0;
-        do {
-            System.out.println(MENU_HELP);
-
-            switch (commandInput.next().toLowerCase()) {
-                case CMD_GENERATE -> configurePasswords();
-                case CMD_EXIT -> i = 1;
-                default -> System.out.println(INVALID_CMD);
-            }
-        } while (i != 1);
-
-        commandInput.close();
-    }
-
-
-
-    private static final char[] UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-    private static final char[] LOWER = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-    private static final char[] DIGITS = "0123456789".toCharArray();
-    private static final char[] SPECIAL = "#*+-_!$&=?".toCharArray();
-
-
-    static int validatePositiveInteger(String receivedInteger) {
-
-        int trueInteger;
-        try {
-            trueInteger = Integer.parseInt(receivedInteger);
-        } catch (NumberFormatException e) {
-            System.out.println(ANSI_RED + ERROR_POSITIVE_INTEGER + ANSI_RESET);
-            return -1;
-        }
-        if (trueInteger <= 0) {
-            System.out.println(ANSI_RED + ERROR_POSITIVE_INTEGER + ANSI_RESET);
-            return -1;
-        }
-        return trueInteger;
-    }
-
-
-
-    static void configurePasswords() {
-        int pwLength = -1, pwCount, mode;
         Scanner input = new Scanner(System.in);
+        while (true) {
 
-        System.out.print(ASK_LENGTH);
-        pwLength = validatePositiveInteger(input.next());
+            System.out.println(MAIN_MENU);
+            String userInput = input.nextLine().toLowerCase();
 
-        if (pwLength == -1 ) {
-            return;
-        }
-        if (pwLength < 4 || pwLength > 16) {
-            System.out.println(ANSI_RED + ERROR_LENGTH + ANSI_RESET);
-            return;
-        }
+            switch (userInput) {
 
-        System.out.print(ASK_COUNT);
-        pwCount = validatePositiveInteger(input.next());
-        if (pwCount <= 0) return;
+                case COMMAND_GENERATE -> configurePasswordList();
+                case COMMAND_EXIT     -> { input.close(); parameters.close(); System.exit(0); }
+                default               -> System.out.println(QUOTES + userInput + QUOTES + COMMAND_ERROR);
 
-        System.out.println(MODE_SELECTION);
-        System.out.print(ASK_MODE);
-        mode = validatePositiveInteger(input.next());
-        if (mode != 1 && mode != 2) {
-            System.out.println(ANSI_RED + ERROR_MODE + ANSI_RESET);
-            return;
+            }
+
         }
 
-
-        StringBuilder selectedChars = new StringBuilder();
-        for (char c : UPPER) selectedChars.append(c);
-        for (char c : LOWER) selectedChars.append(c);
-        for (char c : SPECIAL) if (mode == 2) selectedChars.append(c);
-
-        char[] charSet = selectedChars.toString().toCharArray();
-
-
-        int requiredDigits = switch (pwLength) {
-            case 4, 5, 6 -> 2;
-            case 7, 8, 9, 10 -> 3;
-            default -> 4;
-        };
-
-        System.out.println(passwordsToString(
-                generatePasswords(pwLength, pwCount, charSet, requiredDigits)
-        ));
-
-
-        System.out.println("Es wurden " + pwCount + " Passwörter, jeweils " + pwLength + " Zeichen lang, generiert.\n"
-                + "Dauer der Generierung: " + PW_GENERATED_TIME + " ms, Dauer der Konvertierung: " + PW_TOSTRING_TIME + " ms");
     }
 
-    static char[][] generatePasswords(int pwLength, int pwCount, char[] charSet, int requiredDigits) {
+    static void exportPasswordList(String passwordList) {
+
+        File file = new File(PASSWORDS_FILENAME);
+
+        try (FileWriter writer = new FileWriter(file)) {
+
+            writer.write(passwordList);
+            System.out.println(WRITE_SUCCEEDED + file.getAbsolutePath());
+
+        } catch (IOException e) {
+
+            System.out.println(WRITE_FAILED + e.getMessage());
+
+        }
+
+    }
+
+    static void configurePasswordList() {
+
+        int pwMode, pwLength, pwCount, pwDigitCount = -1;
+        String saveToFile, passwordList = EMPTY_SPACEHOLDER;
+
+        System.out.print(REQUEST_PASSWORD_COUNT);
+        pwCount = validatePositiveInteger(parameters.next());
+        if (pwCount == -1) return;
+
+        System.out.print(REQUEST_PASSWORD_LENGTH);
+        pwLength = validatePositiveInteger(parameters.next());
+        if (pwLength == -1) return;
+        if (pwLength > 16 || pwLength < 4) {
+
+            System.out.println(ERROR_OUT_OF_BOUND_PASSWORD_LENGTH);
+            return;
+
+        }
+
+        switch (pwLength) {
+
+            case 4, 5, 6                -> pwDigitCount = 2;
+            case 7, 8, 9, 10            -> pwDigitCount = 3;
+            case 11, 12, 13, 14, 15, 16 -> pwDigitCount = 4;
+
+        }
+
+        System.out.print(REQUEST_PASSWORD_MODE);
+        pwMode = validatePositiveInteger(parameters.next());
+        if (pwMode == -1) return;
+        if (pwMode > 2 || pwMode < 1) {
+
+            System.out.println(ERROR_MODE_TYPE);
+            return;
+
+        }
+
+        for (int i = 0; i < pwCount; i++) {
+            passwordList += (i + 1) + NUMMERATOR;
+            passwordList += generator(pwDigitCount, pwLength, pwMode) + LINEBREAK;
+        }
 
 
-        long start = System.currentTimeMillis();
+        System.out.print(PRINT_OR_SAVE_PASSWORDS);
+        saveToFile = parameters.next().toUpperCase();
 
-        char[][] generatedPasswords = new char[pwLength][pwCount];
+        if (saveToFile.equals(J)) {
+
+            exportPasswordList(passwordList);
+
+        } else {
+
+            System.out.println(LINEBREAK + passwordList);
+
+        }
+
+    }
+
+    static String generator(int pwDigitCount, int pwLength, int pwMode) {
+
         Random rng = new Random();
 
-        for (int i = 0; i < pwCount; i++) {
-            int digitsPlaced = 0;
-            int[] digitPositions = new int[requiredDigits];
+        String charSet = EMPTY_SPACEHOLDER, password = EMPTY_SPACEHOLDER;
+        String isInteger;
 
-            while (digitsPlaced < requiredDigits) {
-                int pos = rng.nextInt(pwLength);
-                boolean unique = true;
-                for (int j = 0; j < digitsPlaced; j++) {
-                    if (digitPositions[j] == pos) {
-                        unique = false;
-                        break;
-                    }
-                }
-                if (unique) {
-                    digitPositions[digitsPlaced++] = pos;
-                }
-            }
+        if (pwMode == 1) charSet = ALPHANUMMERICAL;
+        if (pwMode == 2) charSet = ALPHANUMMERICAL + SPECIAL;
 
-            for (int j = 0; j < pwLength; j++) {
-                boolean isDigitPos = false;
-                for (int d : digitPositions) {
-                    if (d == j) {
-                        isDigitPos = true;
-                        break;
-                    }
-                }
-                if (isDigitPos) {
-                    generatedPasswords[j][i] = DIGITS[rng.nextInt(DIGITS.length)];
+        for(int i = 0; i < pwLength; i++) {
+
+            if (pwDigitCount > 0) {
+
+                isInteger = EMPTY_SPACEHOLDER;
+                isInteger += charSet.charAt(rng.nextInt(charSet.length()));
+
+                if (isInteger.matches("\\d")) {
+
+                    password += isInteger;
+                    pwDigitCount--;
+
                 } else {
-                    generatedPasswords[j][i] = charSet[rng.nextInt(charSet.length)];
+
+                    password += isInteger;
+
                 }
+
+            } else {
+
+                password += charSet.charAt(rng.nextInt(SKIP_INTEGERS, charSet.length()));
+
             }
+
         }
 
-        long end = System.currentTimeMillis();
-        PW_GENERATED_TIME = end - start;
-        return generatedPasswords;
+        return password;
+
     }
 
-    static String passwordsToString(char[][] generatedPasswords) {
+    static int validatePositiveInteger(String receivedInteger){
 
-        long start = System.currentTimeMillis();
-        int pwLength = generatedPasswords.length;
-        int pwCount = generatedPasswords[0].length;
-        StringBuilder sb = new StringBuilder(pwCount * (pwLength + 10));
-        for (int i = 0; i < pwCount; i++) {
-            sb.append((i + 1)).append(". Passwort: ");
-            for (int j = 0; j < pwLength; j++) {
-                sb.append(generatedPasswords[j][i]);
-            }
-            sb.append('\n');
+        int trueInteger;
+
+        try {
+
+            trueInteger = Integer.parseInt(receivedInteger);
+
+        } catch (NumberFormatException e) {
+
+            System.out.println(ERROR_NOT_AN_POSITIVE_INTEGER);
+            return -1;
+
         }
-        long end = System.currentTimeMillis();
-        PW_TOSTRING_TIME = end - start;
-        return sb.toString();
+
+        if (trueInteger <= 0) {
+
+            System.out.println(ERROR_NOT_AN_POSITIVE_INTEGER);
+            return -1;
+
+        }
+
+        return trueInteger;
+
     }
+
 }
